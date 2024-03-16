195d289de66961d9261625e9e444d9d8ac4d3fe9
doganozgur <doganozgur034@gmail.com>
2024-03-17 00:04:31 +0300

    feat: post feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Form from "@/components/Form";
 import Header from "@/components/layout/Header";
 import LoginModal from "@/components/modals/LoginModal";
+import PostFeed from "@/components/posts/PostFeed";
 
 export default function Home() {
   return (
     <>
-      <LoginModal />
       <Header showBackArrow label="Home" />
       <Form placeholder="What's happening?" />
+      <PostFeed />
     </>
   );
 }
