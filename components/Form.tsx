788865478b1aa424c872e96888f12e21f1633ff7
doganozgur <dogan.ozgur@ddtech.com.tr>
2024-04-23 08:35:16 +0300

    feat: comments

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,11 +1,13 @@
 import React, { useCallback, useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 import useLoginModal from "@/hooks/useLoginModal";
 import useRegisterModal from "@/hooks/useRegisterModal";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import usePosts from "@/hooks/usePosts";
-import toast from "react-hot-toast";
-import axios from "axios";
+import usePost from "@/hooks/usePost";
+
 import Button from "./Button";
 import Avatar from "./Avatar";
 
@@ -21,6 +23,7 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
 
   const { data: currentUser } = useCurrentUser();
   const { mutate: mutatePosts } = usePosts();
+  const { mutate: mutatePost } = usePost(postId as string);
 
   const [body, setBody] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -29,7 +32,9 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
     try {
       setIsLoading(true);
 
-      await axios.post("/api/posts", {
+      const url = isComment ? `/api/comments?postId=${postId}` : "/api/posts";
+
+      await axios.post(url, {
         body,
       });
 
@@ -37,30 +42,37 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
 
       setBody("");
       mutatePosts();
+      mutatePost();
     } catch (err) {
       toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts]);
+  }, [body, mutatePosts, isComment, postId, mutatePost]);
 
   return (
-    <div className="border-b-[1px] border-neutral-800 px-5 py-2">
+    <div className="border-b-[1px] border-neutral-800 px-5 py-4">
       {currentUser ? (
         <div className="flex flex-row gap-4">
-          <div>
-            <Avatar userId={currentUser?.id} />
-          </div>
-          <div className="w-full">
+          <Avatar userId={currentUser?.id} />
+          <div
+            className={`flex-1 py-3 ${
+              isComment && "flex items-center justify-between"
+            }`}
+          >
             <textarea
               disabled={isLoading}
               onChange={(e) => setBody(e.target.value)}
               value={body}
-              className="disabled:opacity-80 peer resize-none mt-3 w-full bg-black ring-0 outline-none text-[20px] placeholder-neutral-500 text-white"
+              className="disabled:opacity-80 peer h-12 resize-none w-full bg-black ring-0 outline-none text-[20px] placeholder-neutral-500 text-white"
               placeholder={placeholder}
             ></textarea>
-            <hr className="opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transitiont" />
-            <div className="mt-4 flex flex-row justify-end">
+            <hr
+              className={`opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition ${
+                isComment && "hidden"
+              }`}
+            />
+            <div className="flex flex-row justify-end">
               <Button
                 disabled={isLoading || !body}
                 onClick={onSubmit}
